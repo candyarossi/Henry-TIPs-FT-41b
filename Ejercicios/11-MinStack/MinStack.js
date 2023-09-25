4b77eeeb6674980a7e0b929830cb9d80db411f75
Candela Yarossi <candyarossi@gmail.com>
2023-09-25 14:43:36 -0300

    Tip 11

--- Ejercicios/11-MinStack/MinStack.js
+++ Ejercicios/11-MinStack/MinStack.js
@@ -1,13 +1,64 @@
 class Node {
   // Your code here:
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+  }
 
+  push(value) {
+    if (!this.top) this.top = new Node(value);
+    else {
+      let nodo = new Node(value);
+      nodo.next = this.top;
+      this.top = nodo;
+    }
+  }
+  pop() {
+    let oldTop = this.top;
+    this.top = oldTop && oldTop.next;
+    return oldTop && oldTop.value;
+  }
+  peek() {
+    return this.top && this.top.value;
+  }
 }
 
-class MinStack {
+class MinStack extends Stack {
   // Your code here:
+  constructor() {
+    super();
+    this.minimum = new Stack();
+  }
+
+  push(value) {
+    if (!this.top) {
+      this.top = new Node(value);
+      this.minimum.push(value);
+    } else {
+      let nodo = new Node(value);
+      nodo.next = this.top;
+      this.top = nodo;
+      if (this.minimum.peek() > value) this.minimum.push(value);
+      else this.minimum.push(this.minimum.peek());
+    }
+  }
+
+  pop() {
+    let oldTop = this.top;
+    this.top = oldTop && oldTop.next;
+    this.minimum.pop();
+    return oldTop && oldTop.value;
+  }
+
+  min() {
+    return this.minimum.peek();
+  }
 }
 
 module.exports = {
