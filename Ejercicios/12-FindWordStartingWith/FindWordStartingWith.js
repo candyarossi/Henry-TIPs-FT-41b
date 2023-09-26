e84d6f984c2405da9653334cb793cb5eb6129d7a
Candela Yarossi <candyarossi@gmail.com>
2023-09-26 15:15:38 -0300

    Tip 12

--- Ejercicios/12-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/12-FindWordStartingWith/FindWordStartingWith.js
@@ -1,5 +1,37 @@
 function FindWordStartingWith(book, query) {
   // Your code here:
+
+  // OPCION 1
+
+  let text = book.text.toLowerCase();
+  query = query.toLowerCase();
+
+  let index = -1;
+  let indices = [];
+
+  do {
+    index = text.indexOf(query, index + 1);
+    if (index >= 0 && (text[index - 1] === " " || index === 0))
+      indices.push(index);
+  } while (index != -1);
+
+  return indices;
+
+  // OPCION 2
+
+  let bookName = book.text.toLowerCase();
+  query = query.toLowerCase();
+  const findWords = [];
+  bookName = bookName.split(" ");
+  let bookNameSearch = bookName.map((word) => word.search(query));
+  let index = 0;
+  for (let i = 0; i < bookNameSearch.length; i++) {
+    if (bookNameSearch[i] === 0) {
+      findWords.push(index);
+    }
+    index = index + bookName[i].length + 1;
+  }
+  return findWords;
 }
 
 module.exports = FindWordStartingWith;
